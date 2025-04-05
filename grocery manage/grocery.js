ce6d64e71ba1da719b5e02b8948dd986504280e7
Altamash Shaikh <mr.shaikh.altamash0203@gmail.com>
2025-04-05 15:10:19 +0530

    mock

---
+++ grocery manage/grocery.js
@@ -0,0 +1,10 @@
+let item=[]
+
+item.push("apples","bananas","bread","milk")
+
+// item.pop()
+
+
+console.log("totalItems:",item.length)
+console.log("items:",item)
+console.log("removed item:",item.pop())
